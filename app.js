8fa71554dc116bec9922e21da54eafc4598d14ae
DMWH-Bsmith <bsmith@dmwandh.com>
2024-12-14 16:25:37 -0500

    idk man

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // ELEMENTS //////////////////////////////////////////////// 
 const introEl = document.querySelector('.intro'); 
 const firstEl = document.querySelector('.first');
+const secondEl = document.querySelector('.second');
 const killsLeftEl = document.querySelector('.killsLeft');
+const inputRowEl = document.querySelector('.inputRow');
+const gameArchiveEl = document.querySelector('.gameArchive');
+const achievedSetEl = document.querySelector('.achievedSet');
 //////////////////////////////////////////////////////////// 
 // BUTTONS //////////////////////////////////////////////// 
 const startBtn = document.querySelector('.startBtn');
+const subSingleScoreBtn = document.createElement('button');
+subSingleScoreBtn.textContent = 'submit';
+subSingleScoreBtn.classList.add('.subSingleScoreBtn');
 
 //////////////////////////////////////////////////////////// 
 // VALUES /////////////////////////////////////////////////
@@ -14,20 +21,34 @@ let killsLeft = 100;
 mapArray = ['Babylon', 'Derelict', 'Gala', 'Lowtown', 
     'Payback', 'Pit', 'Protocol', 'Red card', 'Rewind', 
     'Scud', 'Skyline', 'Stakeout', 'Subsonic', 'Vault', 
-    'Vorkuta', 'Warehead']
+    'Vorkuta', 'Warehead'];
+
+singleGameArray = [];
+totalSetArray = [];
 //////////////////////////////////////////////////////////
-
-killsLeftEl.textContent = killsLeft;
-
 
 
 startBtn.addEventListener('click', () => {
     introEl.classList.add('hide');
     firstEl.classList.remove('hide');
+    killInput.focus();
 })
+
+killsLeftEl.textContent = killsLeft;
 
 
 let mapDropdown = document.createElement('select');
+
+
+// INPUT ROW ELEMENTS ---> .inputRowEl ///////////////////
+let killInput = document.createElement('input');
+killInput.classList.add('killInput');
+let hyph = document.createElement('h3');
+hyph.textContent = ' - ';
+let deathInput = document.createElement('input');
+deathInput.classList.add('deathInput');
+//////////////////////////////////////////////////////////
+
 
 
 for (let i = 0; i <= mapArray.length -1; i++) { 
@@ -37,7 +58,89 @@ for (let i = 0; i <= mapArray.length -1; i++) {
     mapDropdown.appendChild(option); 
 }
 
+inputRowEl.appendChild(killInput);
+inputRowEl.appendChild(hyph);
+inputRowEl.appendChild(deathInput);
 firstEl.appendChild(mapDropdown);
+firstEl.appendChild(inputRowEl);
+firstEl.appendChild(subSingleScoreBtn);
+firstEl.appendChild(gameArchiveEl);
+
+killInput.addEventListener('input', () => {
+    if (killInput.value.length > 1) {
+        deathInput.focus();
+    }
+})
+
+killInput.addEventListener('keydown', (e) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+        deathInput.focus();
+    }
+})
+
+deathInput.addEventListener('input', () => {
+    if (deathInput.value.length > 1) {
+        killsLeft -= killInput.value;
+        killsLeftEl.textContent = killsLeft;
+        singleGameArray.push({ 
+        kills: killInput.value, 
+        deaths: deathInput.value
+    });
+    if (killsLeft <= 0) {
+        firstEl.classList.add('hide');
+        secondEl.classList.remove('hide');
+        let totalKills = killsLeft * -1 + 100;
+        let totalDeaths 
+        totalSetArray.push({ 
+            totalKills: totalKills,
+            totalDeaths: deathInput.value,
+            games: {singleGameArray}
+        })
+        console.log(totalSetArray.games.length);
+        console.log(totalSetArray);
+    } else {
+        killInput.value = '';
+        deathInput.value = '';
+        killInput.focus();
+    }
+    
+    }
+})
+
+deathInput.addEventListener('keydown', (e) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+            killsLeft -= killInput.value;
+            killsLeftEl.textContent = killsLeft;
+            singleGameArray.push({ 
+                kills: killInput.value, 
+                deaths: deathInput.value
+            })
+            if (killsLeft <= 0) {
+                firstEl.classList.add('hide');
+                secondEl.classList.remove('hide');
+            } else {
+                killInput.value = '';
+                deathInput.value = '';
+                killInput.focus();
+            }
+        }    
+    }
+);
+
+    subSingleScoreBtn.addEventListener('click', () => {
+        killsLeft -= killInput.value;
+        killsLeftEl.textContent = killsLeft;
+        singleGameArray.push({ 
+            kills: killInput.value, 
+            deaths: deathInput.value
+        });
+        killInput.value = '';
+        deathInput.value = '';
+        killInput.focus();
+})
+
+
+console.log(singleGameArray);
 
 
 
